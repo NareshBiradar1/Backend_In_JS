980ada1a37fff3fcede18b891e734bc14021da69
NareshBiradar1 <nareshdb555@gmail.com>
2024-07-03 10:41:20 +0530

    http methods added

--- Express_start/index.js
+++ Express_start/index.js
@@ -7,8 +7,40 @@ let courses  = [
     {id:3 , name:"python"}
 ]
 
+app.use(express.json());
+
 app.get('/courses' , (req,res)=>{
     res.json(courses);
+})
+
+app.post('/courses' , (req,res)=>{
+    let newCourse  = {
+        id : courses.length+1 ,
+        name : req.body.name
+    }
+    courses.push(newCourse);
+    res.json(courses);
+});
+
+app.put('/courses/:id' , (req,res)=>{
+    const ID = req.params.id;
+
+    let course = courses.find(course => course.id === ID);
+
+    if(!course){
+        res.send("course not found");
+    }
+    else{
+        course.name = req.body.name;
+        res.send(courses)
+    }
+})
+
+app.delete('/courses/:id' , (req,res)=>{
+    const ID = req.params.id;
+
+    courses = courses.filter(course => course.id !== ID)
+    res.send(courses);
 })
 
 app.listen(3000 , ()=>{
